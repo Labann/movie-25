a772463a0060d29291533d8694a24a91d25c59c4
Labann <labanmusen32@gmail.com>
2025-11-13 14:13:24 +0300

    actor section

--- client/components/Actor.tsx
+++ client/components/Actor.tsx
@@ -7,7 +7,7 @@ export const ActorLoader = () => {
   return (
     <div className='w-[9em] p-1 h-[15em]'>
       <div className="bg-gray-light w-[7em] h-[7em] rounded-full animate-pulse"></div>
-      <div className="w-3/4 mx-auto bg-gray-light mt-2 animate-pulse"></div>
+      <div className="w-3/4 mx-auto bg-gray-light mt-2 h-2 animate-pulse"></div>
       </div>
   )
 }
@@ -15,7 +15,7 @@ const Actor = ({name, img}: {name: string, img: string}) => {
   return (
     <div className="w-[9em] p-2 flex-col text-center space-y-2">
             {img && <Image 
-              src={`${ImgUrl}w-185${img}`}
+              src={`${ImgUrl}w185${img}`}
               alt='thumbnail'
               unoptimized
               width={100}
