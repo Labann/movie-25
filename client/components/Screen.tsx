004f1360f57e8ef43e87eea5fab025d3739422da
Labann <labanmusen32@gmail.com>
2025-11-13 17:51:33 +0300

    screen done

--- client/components/Screen.tsx
+++ client/components/Screen.tsx
@@ -1,22 +1,22 @@
 "use client"
-import React, { useEffect } from 'react'
-import ReactPlayer from "react-player"
+import  { useEffect } from 'react'
 import { useAppDispatch, useAppSelector } from '@/app/hooks/redux';
 import { ImgUrl } from '@/app/util/config';
 import { fetch_movie_on_view } from '@/app/store/movieSlice';
-import {
-  MediaController,
-  MediaControlBar,
-  MediaTimeRange,
-  MediaTimeDisplay,
-  MediaVolumeRange,
-  MediaPlaybackRateButton,
-  MediaPlayButton,
-  MediaSeekBackwardButton,
-  MediaSeekForwardButton,
-  MediaMuteButton,
-  MediaFullscreenButton,
-} from "media-chrome/react";
+import dynamic from "next/dynamic";
+
+
+//Dynamically import ReactPlayer to fix typing + SSR
+const ReactPlayer = dynamic(() => import("react-player"), {
+  ssr: false,
+}) as unknown as React.FC<{
+  url: string;
+  controls?: boolean;
+  width?: string;
+  height?: string;
+}>;
+
+
 const Screen = ({movie_id}: {movie_id: string}) => {
     const {movie, movie_on_view} = useAppSelector(state => state.movie);
     const dispatch = useAppDispatch();
@@ -24,40 +24,18 @@ const Screen = ({movie_id}: {movie_id: string}) => {
         dispatch(fetch_movie_on_view({movie_id}))
     }, [movie_id, dispatch])
     return (
-    <div className='w-full relative pt-34 p-4 min-h-[70vh] bg-cover bg-center' style={{backgroundImage: `url(${ImgUrl}w700${movie?.poster_path})`}}
+    <div className='w-full relative pt-34 p-4 min-h-[70vh] bg-cover bg-center' 
+        style={{backgroundImage: `url(${ImgUrl}w700${movie?.poster_path})`}}
     >
         <div className="absolute top-0 left-0 w-full h-full bg-black/50 z-5"></div>
         <div className="max-w-lg mx-auto">
-            <MediaController
-            style={{
-                width: "100%",
-                aspectRatio: "16/9",
-            }}
-                    >
             <ReactPlayer
-                slot="media"
-                src={`https://www.youtube.com/watch?v=${movie_on_view[0]?.key}`}
-                controls={false}
-                style={{
-                width: "100%",
-                height: "100%",
-                }}
-            ></ReactPlayer>
-            <MediaControlBar>
-                <MediaPlayButton />
-                <MediaSeekBackwardButton seekOffset={10} />
-                <MediaSeekForwardButton seekOffset={10} />
-                <MediaTimeRange />
-                <MediaTimeDisplay showDuration />
-                <MediaMuteButton />
-                <MediaVolumeRange />
-                <MediaPlaybackRateButton />
-                <MediaFullscreenButton />
-            </MediaControlBar>
-            </MediaController>
+                url={`https://www.youtube.com/watch?v=${movie_on_view[0]?.key}`}
+                controls
+                width="100%"
+                height="100%"
+                />
         </div>
-        
-        
     </div>
   )
 }
