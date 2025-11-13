d9252c76be41bc283d63e03b5329ef9e1962b413
Labann <labanmusen32@gmail.com>
2025-11-13 20:48:52 +0300

    slide show done

--- client/components/SlideShow.tsx
+++ client/components/SlideShow.tsx
@@ -16,9 +16,9 @@ const SlideShow = ({title}: {
   return (
     <div className='p-4 max-w-7xl mx-auto pb-14 pt-6'>
         <h3 className='font-bold text-2xl text-white capitalize pb-2'>{title}</h3>
-        <div className="overflow-x-scroll scroll-none w-[50w] mx-auto">
+        <div className="overflow-x-scroll scroll-none w-[80w] mx-auto">
             <div className="flex items-center space-x-4 w-fit">
-                {isLoading && ["1", 2, 3].map(item => <CardLoader key={item}/>)}
+                {isLoading && ["1", 2, 3, 4, 6, 7].map(item => <CardLoader key={item}/>)}
                 {!isLoading && title === "Trending" && trending?.map(movie => <Card key={movie.id} movie={movie}/>)}
                 {!isLoading && title === "Popular searches" && popular?.map(movie => <Card key={movie.id} movie={movie}/>)}
             </div>
