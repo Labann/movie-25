2cfdb32d1b08f3e91c3b08d57b8685c1325d4955
Labann <labanmusen32@gmail.com>
2025-11-13 16:36:51 +0300

    react player

--- client/components/Screen.tsx
+++ client/components/Screen.tsx
@@ -4,6 +4,19 @@ import ReactPlayer from "react-player"
 import { useAppDispatch, useAppSelector } from '@/app/hooks/redux';
 import { ImgUrl } from '@/app/util/config';
 import { fetch_movie_on_view } from '@/app/store/movieSlice';
+import {
+  MediaController,
+  MediaControlBar,
+  MediaTimeRange,
+  MediaTimeDisplay,
+  MediaVolumeRange,
+  MediaPlaybackRateButton,
+  MediaPlayButton,
+  MediaSeekBackwardButton,
+  MediaSeekForwardButton,
+  MediaMuteButton,
+  MediaFullscreenButton,
+} from "media-chrome/react";
 const Screen = ({movie_id}: {movie_id: string}) => {
     const {movie, movie_on_view} = useAppSelector(state => state.movie);
     const dispatch = useAppDispatch();
@@ -14,15 +27,36 @@ const Screen = ({movie_id}: {movie_id: string}) => {
     <div className='w-full relative pt-34 p-4 min-h-[70vh] bg-cover bg-center' style={{backgroundImage: `url(${ImgUrl}w700${movie?.poster_path})`}}
     >
         <div className="absolute top-0 left-0 w-full h-full bg-black/50 z-5"></div>
+        <div className="max-w-lg mx-auto">
+            <MediaController
+            style={{
+                width: "100%",
+                aspectRatio: "16/9",
+            }}
+                    >
+            <ReactPlayer
+                slot="media"
+                src="https://stream.mux.com/maVbJv2GSYNRgS02kPXOOGdJMWGU1mkA019ZUjYE7VU7k"
+                controls={false}
+                style={{
+                width: "100%",
+                height: "100%",
+                }}
+            ></ReactPlayer>
+            <MediaControlBar>
+                <MediaPlayButton />
+                <MediaSeekBackwardButton seekOffset={10} />
+                <MediaSeekForwardButton seekOffset={10} />
+                <MediaTimeRange />
+                <MediaTimeDisplay showDuration />
+                <MediaMuteButton />
+                <MediaVolumeRange />
+                <MediaPlaybackRateButton />
+                <MediaFullscreenButton />
+            </MediaControlBar>
+            </MediaController>
+        </div>
         
-        <ReactPlayer
-          width={'100%'}
-          height={"100%"}
-          className='mx-auto'
-          controls={true}
-          playing={true}
-          src={`https://www.youtube.com/watch?v=${movie_on_view[0]?.key}`}
-        />
         
     </div>
   )
