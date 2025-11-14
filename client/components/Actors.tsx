3e8ebe55e87b17303fd2880860c6000ec3707b3e
Labann <labanmusen32@gmail.com>
2025-11-14 16:23:56 +0300

    actors screen

--- client/components/Actors.tsx
+++ client/components/Actors.tsx
@@ -9,7 +9,7 @@ const Actors = ({title}: {title: string}) => {
   return (
     <div className='max-w-7xl mx-auto py-4 p-4'>
       <h3 className='text-lg text-white capitalize font-bold'>{title}</h3>
-      <div className="w-[90vw] xl:w-[85vw] overflow-x-scroll scroll-none">
+      <div className="w-[90vw] overflow-x-scroll scroll-none">
         <div className="flex items-center pt-3 space-x-3 w-fit">
           {isLoading || loadingPerson && [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].map(item => <ActorLoader key={item}/>)}
           {!loadingPerson && title === "Most Famous actor globally" && person.map(actor => <Actor key={actor.id} name={actor.original_name} img={actor.profile_path}/>)}
