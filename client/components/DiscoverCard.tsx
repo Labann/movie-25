86767aea10237b78e4dbd1331d83a4f5c0715aa2
Labann <labanmusen32@gmail.com>
2025-11-14 16:32:31 +0300

    nav bar remaded

--- client/components/DiscoverCard.tsx
+++ client/components/DiscoverCard.tsx
@@ -13,14 +13,17 @@ import Link from 'next/link';
 const DiscoverCard = ({movie}: {movie: IMovie}) => {
   return (
     <div className='rounded-md p-2 max-w-xl bg-gray-neutral overflow-hidden'>
-        {movie.poster_path && <Image
-            alt="discover-card"
-            width={300}
-            height={300}
-            src={`${ImgUrl}w780${movie.poster_path}`}
-            unoptimized
-            className='w-full object-fit rounded-md hover:scale-105 h-[20em] object-cover'
-        />}
+        <div className="overflow-hidden">
+            {movie.poster_path && <Image
+                    alt="discover-card"
+                    width={300}
+                    height={300}
+                    src={`${ImgUrl}w780${movie.poster_path}`}
+                    unoptimized
+                    className='w-full object-fit rounded-md hover:scale-105 h-[10em] object-cover'
+                />}
+        </div>
+        
         <div className="flex flex-col space-y-6">
         <h1 className='capitalize text-lg md:text-5xl'>{movie.title}</h1>
         <div className="flex space-x-2 font-bold items-center text-xs">
@@ -31,16 +34,14 @@ const DiscoverCard = ({movie}: {movie: IMovie}) => {
             <GoDotFill/>
             <p>{formatRuntimeSafe(movie.runtime)}</p>
         </div>
-        <p className='text-gray-300 text-md'>{movie.overview}</p>
+        <div className="overflow-y-auto h-[10em] scroll-none">
+            <p className='text-gray-300 text-md'>{movie.overview}</p>
+        </div>
         <div className="pt-4 flex space-x-3 items-center">
             <Link href={`/watching_screen/${movie.id}`} className='bg-gray-neutral border-gray-semibold border hover:bg-gray-light cursor-pointer flex text-sm items-center px-6 py-2 space-x-2 rounded-2xl capitalize text-white'>
                 <FaPlay />
                 <p>Play Now</p>
             </Link>
-            
-            <button className='cursor-pointer text-md border-gray-semibold border rounded-full p-1'><FaPlus size={'1.5em'}/>{""}</button>
-            <button className='cursor-pointer text-md border-gray-semibold border rounded-full p-1'><IoIosShareAlt size={"1.5em"}/>{""}</button>
-
         </div>  
     </div>
     </div>
