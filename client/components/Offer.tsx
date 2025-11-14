61f63a12ce8c34e8e0747095a41d1675befe48a1
Labann <labanmusen32@gmail.com>
2025-11-14 11:44:57 +0300

    polish 102

--- client/components/Offer.tsx
+++ client/components/Offer.tsx
@@ -2,9 +2,8 @@ import React from 'react'
 import OfferCard from './OfferCard'
 const Offers = () => {
   return (
-    <div className="p-4 bg-primary min-h-[50vh] pt-10">
+    <div className="p-4 bg-primary py-4">
        <div className='max-w-7xl mx-auto'>
-          <div className="">
               <h3 className='text-white font-bold text-xl text-center'>What we offer</h3>
               <div className="grid grid-cols-2 w-fit md:w-auto mx-auto md:grid-cols-4 gap-4 space-x-2 py-6">
                 <OfferCard title="Movie" img={"/movie.jpg"}/>
@@ -12,8 +11,6 @@ const Offers = () => {
                 <OfferCard title='Animation' img={"/animation.jpg"}/>
                 <OfferCard title="Documentaries" img={"/documentary.jpg"}/>
               </div>
-          </div>
-          
       </div>
     </div>
    
--- client/components/OfferCard.tsx
+++ client/components/OfferCard.tsx
@@ -4,13 +4,15 @@ import Link from 'next/link'
 const OfferCard = ({title, img}: {title: string, img: string}) => {
   return (
     <Link href={`/discover/${title}`} className="w-fit">
-          <Image
-            src={img}
-            alt='card_img'
-            width={250}
-            height={250}
-            className='rounded-md h-[15em] object-fit object-contain hover:scale-105 cursor-pointer'
-          />
+          <div className="overflow-hidden mx-auto">
+            <Image
+              src={img}
+              alt='card_img'
+              width={250}
+              height={250}
+              className='rounded-md h-[18em] object-fit object-cover hover:scale-105 cursor-pointer'
+            />
+          </div>
           <div className="text-center text-gray-light font-bold py-3">{title}</div>
         </Link>
   )
