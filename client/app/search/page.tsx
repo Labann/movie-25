7fa4af2b8fa184c897af66b8bbd9cffde1d361d5
Labann <labanmusen32@gmail.com>
2025-11-14 17:09:50 +0300

    removed protect from search endpoint

--- client/app/search/page.tsx
+++ client/app/search/page.tsx
@@ -7,12 +7,14 @@ import DiscoverCard from '@/components/DiscoverCard'
 const Search = () => {
   const {search_results, isSuccess} = useAppSelector(state => state.movie);
   return (
-    <div className='p-4 bg-primary pt-32 min-h-screen max-w-7xl mx-auto flex flex-col space-y-2'>
-      <SearchInput/>
-      {isSuccess && search_results.length === 0 && <RecentlyVisited/>}
-      {search_results.length === 0 && <p className='font-semibold text-gray-light text-lg'>no results found</p>}
-      {search_results.length !== 0 && search_results.map(res => <DiscoverCard key={res.id} movie={res}/>)}
-    </div>
+    <div className="bg-primary">
+      <div className='p-4 pt-32 min-h-screen max-w-7xl mx-auto flex flex-col space-y-2'>
+        <SearchInput/>
+        {isSuccess && search_results.length === 0 && <RecentlyVisited/>}
+        {search_results.length === 0 && <p className='font-semibold text-gray-light text-lg'>no results found</p>}
+        {search_results.length !== 0 && search_results.map(res => <DiscoverCard key={res.id} movie={res}/>)}
+      </div>
+    </div> 
   )
 }
 
