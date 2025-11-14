090f834fda5924310e7d1c1f148336adb0b20d7b
Labann <labanmusen32@gmail.com>
2025-11-14 10:42:52 +0300

    reviews done

--- client/components/Reviews.tsx
+++ client/components/Reviews.tsx
@@ -15,7 +15,8 @@ const Reviews = ({movie_id}:{
     <div className='max-w-7xl mx-auto p-4'>
         <h2 className='font-bold text-lg text-white'>Reviews</h2>
         <div className="grid grid-cols-2 scroll-none gap-3 space-x-3 pt-4 overflow-x-scroll">
-            {reviews.map(review => <Review key={review.id} review={review}/>)}
+            {reviews.length === 0 && <p className='font-bold text-sm'>no reviews</p>}
+            {reviews.length !== 0 && reviews.map(review => <Review key={review.id} review={review}/>)}
         </div>
         
     </div>
