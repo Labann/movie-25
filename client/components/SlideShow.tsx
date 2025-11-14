e0fce6bbabeb9e64b5cf39de45df1d3840a09623
Labann <labanmusen32@gmail.com>
2025-11-14 11:57:57 +0300

    reduced padding on slide show

--- client/components/SlideShow.tsx
+++ client/components/SlideShow.tsx
@@ -14,7 +14,7 @@ const SlideShow = ({title}: {
     dispatch(fetchPopular())
   }, [dispatch])
   return (
-    <div className='p-4 max-w-7xl mx-auto pb-14 pt-6'>
+    <div className='p-4 max-w-7xl mx-auto py-6'>
         <h3 className='font-bold text-2xl text-white capitalize pb-2'>{title}</h3>
         <div className="overflow-x-scroll scroll-none w-[80w] mx-auto">
             <div className="flex items-center space-x-4 w-fit">
