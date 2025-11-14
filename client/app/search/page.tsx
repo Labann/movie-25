7be6bd7d83436bd686d3142f63b5c311cf6b15ad
Labann <labanmusen32@gmail.com>
2025-11-14 17:16:18 +0300

    polished search page

--- client/app/search/page.tsx
+++ client/app/search/page.tsx
@@ -8,11 +8,16 @@ const Search = () => {
   const {search_results, isSuccess} = useAppSelector(state => state.movie);
   return (
     <div className="bg-primary">
-      <div className='p-4 pt-32 min-h-screen max-w-7xl mx-auto flex flex-col space-y-2'>
+      <div className='p-4 pt-32 min-h-screen max-w-7xl mx-auto space-y-2'>
         <SearchInput/>
+        
         {isSuccess && search_results.length === 0 && <RecentlyVisited/>}
-        {search_results.length === 0 && <p className='font-semibold text-gray-light text-lg'>no results found</p>}
-        {search_results.length !== 0 && search_results.map(res => <DiscoverCard key={res.id} movie={res}/>)}
+        <div className="grid grid-cols-12">
+          {search_results.length === 0 && <p className='font-semibold text-gray-light text-lg col-span-10'>no results found</p>}
+          <div className="grid w-fit mx-auto sm:grid-cols-2 sm:w-auto col-span-12">
+              {search_results.length !== 0 && search_results.map(res => <DiscoverCard key={res.id} movie={res}/>)}
+          </div>
+        </div>
       </div>
     </div> 
   )
