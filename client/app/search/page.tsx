7c470675e9ee51069b45e7e81a12d5442f033d71
Labann <labanmusen32@gmail.com>
2025-11-14 17:04:54 +0300

    search results

--- client/app/search/page.tsx
+++ client/app/search/page.tsx
@@ -5,11 +5,11 @@ import RecentlyVisited from '@/components/RecentlyVisited'
 import { useAppSelector } from '../hooks/redux'
 import DiscoverCard from '@/components/DiscoverCard'
 const Search = () => {
-  const {search_results} = useAppSelector(state => state.movie);
+  const {search_results, isSuccess} = useAppSelector(state => state.movie);
   return (
-    <div className='p-4 bg-primary pt-32 min-h-screen'>
+    <div className='p-4 bg-primary pt-32 min-h-screen max-w-7xl mx-auto'>
       <SearchInput/>
-      {search_results.length === 0 && <RecentlyVisited/>}
+      {isSuccess && search_results.length === 0 && <RecentlyVisited/>}
       {search_results.length === 0 && <p className='font-semibold text-gray-light text-lg'>no results found</p>}
       {search_results.length !== 0 && search_results.map(res => <DiscoverCard key={res.id} movie={res}/>)}
     </div>
