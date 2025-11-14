86349049bc18c0251dc31e41528b84c73d7fabaa
Labann <labanmusen32@gmail.com>
2025-11-14 11:33:15 +0300

    p4 movie_details

--- client/components/Movie_details.tsx
+++ client/components/Movie_details.tsx
@@ -16,8 +16,8 @@ const Movie_details = ({movie_id}: {movie_id: string}) => {
     }, [movie_id, dispatch])
    
   return (
-    <div className="bg-primary py-10 p-4">
-        <div className='mx-auto max-w-7xl'>
+    <div className="bg-primary py-10">
+        <div className='mx-auto max-w-7xl p-4'>
             <h3 className='text-gray-semibold'>More details</h3>
             <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-3">
                 <div className="flex flex-col space-y-3">
