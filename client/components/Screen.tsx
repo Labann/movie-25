814f9c2c1ed3bd2be02f75bc80cd6b9860a94cda
Labann <labanmusen32@gmail.com>
2025-11-14 09:37:00 +0300

    player done z-index

--- client/components/Screen.tsx
+++ client/components/Screen.tsx
@@ -4,19 +4,7 @@ import { useAppDispatch, useAppSelector } from '@/app/hooks/redux';
 import { ImgUrl } from '@/app/util/config';
 import { fetch_movie_on_view } from '@/app/store/movieSlice';
 import dynamic from "next/dynamic";
-import {
-  MediaController,
-  MediaControlBar,
-  MediaTimeRange,
-  MediaTimeDisplay,
-  MediaVolumeRange,
-  MediaPlaybackRateButton,
-  MediaPlayButton,
-  MediaSeekBackwardButton,
-  MediaSeekForwardButton,
-  MediaMuteButton,
-  MediaFullscreenButton,
-} from "media-chrome/react";
+
 
 //Dynamically import ReactPlayer to fix typing + SSR
 const ReactPlayer = dynamic(() => import("react-player"), {
@@ -43,32 +31,14 @@ const Screen = ({movie_id}: {movie_id: string}) => {
     >
         <div className="absolute top-0 left-0 w-full h-full bg-black/50 z-5"></div>
             <div className="md:h-[500px] h-[300px] max-w-7xl mx-auto">
-                 <MediaController
-                    style={{
-                        width: "100%",
-                        aspectRatio: "16/9",
-                    }}
-                    >
-                        <ReactPlayer
-                            width={'100%'}
-                            height='100%'
-                            controls={true}
-                            playing={true}
-                            className='w-fit border border-gray-light rounded-md'
-                            src={`https://www.youtube.com/watch?v=${movie_on_view[0]?.key}`}
-                        />
-                </MediaController>
-                <MediaControlBar className='flex space-x-2 items-center'>
-                <MediaPlayButton />
-                    <MediaSeekBackwardButton seekOffset={10} />
-                    <MediaSeekForwardButton seekOffset={10} />
-                    <MediaTimeRange />
-                    <MediaTimeDisplay showDuration />
-                <MediaMuteButton />
-                    <MediaVolumeRange />
-                    <MediaPlaybackRateButton />
-                    <MediaFullscreenButton />
-                </MediaControlBar>
+                <ReactPlayer
+                    width={'100%'}
+                    height='100%'
+                    controls={true}
+                    playing={true}
+                    className='w-fit border border-gray-light rounded-md z-10'
+                    src={`https://www.youtube.com/watch?v=${movie_on_view[0]?.key}`}
+                />
         </div>
     </div>
   )
