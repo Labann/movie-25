a8ed4bfe087e8e927862d22ebc6137c23cc68288
Labann <labanmusen32@gmail.com>
2025-11-15 12:40:12 +0300

    more details polished

--- client/components/Movie_details.tsx
+++ client/components/Movie_details.tsx
@@ -16,21 +16,21 @@ const Movie_details = ({movie_id}: {movie_id: string}) => {
     }, [movie_id, dispatch])
    
   return (
-    <div className="bg-primary py-4">
+    <div className="bg-primary pt-4">
         <div className='mx-auto max-w-7xl p-4'>
             <h3 className='text-gray-semibold'>More details</h3>
             <div className="grid lg:grid-cols-4 md:grid-cols-3 grid-cols-2 gap-3">
                 <div className="flex flex-col space-y-3">
                     <div className="flex items-center space-x-2 text-gray-400 text-lg">
                         <IoCalendarClearOutline className=' font-semibold'/>
-                        <p className='capitalized'>Released Year</p>
+                        <p className='capitalized text-lg'>Released Year</p>
                     </div>
                     <p className='text-white font-extralight'>{movie?.release_date.slice(0, 4)}</p>
                 </div>
                 <div className="flex flex-col space-y-3">
                     <div className="flex items-center space-x-2 text-gray-400 text-lg">
                         <IoLanguageOutline className=' font-semibold'/>
-                        <p className='capitalized'>Available languages</p>
+                        <p className='capitalized text-lg'>Available languages</p>
                     </div>
                     <div className="flex items-center space-x-2 text-white">
                         {<span className='bg-black rounded-md px-3 py-1 cursor-pointer text-sm'>{movie?.original_language}</span>} 
@@ -39,7 +39,7 @@ const Movie_details = ({movie_id}: {movie_id: string}) => {
                 <div className="flex flex-col space-y-3">
                     <div className="flex items-center space-x-2 text-gray-400 text-lg">
                         <LiaIconsSolid className=' font-semibold'/>
-                        <p className='capitalized'>Genres</p>
+                        <p className='capitalized text-lg'>Genres</p>
                     </div>
                     <div className="flex items-center flex-wrap space-y-2 space-x-2 text-white">
                         {movie?.genres?.map(genre => <span key={genre.id} className='bg-black text-sm rounded-md px-3 py-1 cursor-pointer'>{genre.name}</span>)}
@@ -48,7 +48,7 @@ const Movie_details = ({movie_id}: {movie_id: string}) => {
                 <div className="flex flex-col space-y-3">
                     <div className="flex items-center space-x-2 text-gray-400 text-lg">
                         <GoStarFill className=''/>
-                        <p className='capitalized'>Popularity</p>
+                        <p className='capitalized text-lg'>Popularity</p>
                     </div>
                     <div className="flex items-center space-x-2">
                         <span className='text-xs text-gray-light'>rating</span>
