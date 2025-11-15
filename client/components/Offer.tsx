66b80e675c6ea1db69afe441ddcbb4174ac8f075
Labann <labanmusen32@gmail.com>
2025-11-15 11:19:28 +0300

    lower case title

--- client/components/Offer.tsx
+++ client/components/Offer.tsx
@@ -4,7 +4,7 @@ const Offers = () => {
   return (
     <div className="p-4 bg-primary pt-10">
        <div className='max-w-7xl mx-auto'>
-              <h3 className='text-white font-bold text-xl text-center'>What we offer</h3>
+              <h3 className='text-white font-bold text-lg lowercase text-center'>What we offer</h3>
               <div className="grid grid-cols-2 w-fit md:w-auto mx-auto md:grid-cols-4 gap-4 space-x-2 py-6">
                 <OfferCard title="Movie" img={"/movie.jpg"}/>
                 <OfferCard title='Drama' img={"/series.jpg"}/>
