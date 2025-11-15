c7d203e3b246958dcf854995a2c4ade37b0caad2
Labann <labanmusen32@gmail.com>
2025-11-15 11:52:33 +0300

    nav bar done

--- client/app/search/page.tsx
+++ client/app/search/page.tsx
@@ -9,8 +9,8 @@ const Search = () => {
   return (
     <div className="bg-primary">
       <div className='p-4 pt-32 min-h-screen max-w-7xl mx-auto space-y-2'>
-        <SearchInput/>
-        {isSuccess && search_results.length === 0 && <RecentlyVisited/>}
+        {<SearchInput/>}
+        {/*isSuccess && search_results.length === 0 && <RecentlyVisited/>*/}
         <div className="grid grid-cols-12">
           <div className="grid w-fit mx-auto sm:grid-cols-2 gap-4 sm:w-auto col-span-12 pt-6">
               {isLoading && [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(v => <DiscoverCardLoader key={v}/>)}
--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, {useState} from 'react'
+import React, {useEffect, useRef, useState} from 'react'
 import Image from 'next/image'
 import { CiHome } from "react-icons/ci";
 import { IoIosSearch } from "react-icons/io";
@@ -7,58 +7,101 @@ import { FaPlus } from "react-icons/fa6";
 import Link from 'next/link';
 import { useAppDispatch, useAppSelector } from '@/app/hooks/redux';
 import { logout } from '@/app/store/authSlice';
+import { IoMdMenu } from "react-icons/io";
+import { IoMdClose } from "react-icons/io";
 const Navbar = () => {
   const {currentUser} = useAppSelector(state => state.auth);
   const dispatch = useAppDispatch();
+  const [isOpen, setIsOpen] = useState(false);
+  const navRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (navRef.current && !navRef.current.contains(event.target as Node)) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
   return (
-    <div className={`${currentUser? `bg-primary`: `bg-default/60`} z-50 md:px-6 pr-2 fixed top-0 left-0 w-full flex items-center justify-between`}>
-        <Link href={"/"} className="">
-            <Image
-                src={"/logo_.png"}
-                alt='logo'
-                width={150}
-                height={150}
-            />
-        </Link>
-        {
-          
-            <div className="flex items-center mx-auto md:space-x-6 text-sm space-x-2 text-white">
-              <Link href={"/home"}  className="flex space-x-2 items-center  focus:text-gray-semibold hover:text-gray-semibold">
-                <CiHome size={"1.2em"}/>
-                <span>Home</span>
-              </Link>
-              <Link href={"/search"} className="flex space-x-2 items-center  focus:text-gray-semibold hover:text-gray-semibold">
-                <IoIosSearch size={"1.2em"}/>
-                <span>Search</span>
-              </Link>
-              {currentUser && <Link href={"/watch_list"} className="flex space-x-2 items-center  focus:text-gray-semibold hover:text-gray-semibold">
-                <FaPlus size={"1.2em"}/>
-                <span>Watchlist</span>
-              </Link>}
-            </div>
-          
-        }
-        {
-          currentUser && (
-            <Link href={"/profile"} className="flex space-x-1 text-white items-center">
-              <span>My Profile</span>
+    <div ref={navRef}  className={`${currentUser? `bg-primary`: `bg-default/60`} z-50 md:px-6 pr-2 fixed top-0 left-0 w-full flex flex-col`}>
+        <div className={`flex items-center justify-between w-full`}>
+          {
+            isOpen? <IoMdMenu 
+                    onClick={() => setIsOpen(!isOpen)} 
+                    className='md:hidden cursor-pointer'/>
+                    :<IoMdClose 
+                    onClick={() => setIsOpen(!isOpen)} 
+                    className='md:hidden cursor-pointer'/>
+          }
+          <Link href={"/"} className="">
               <Image
-                src={"/avatar.png"}
-                alt='avatar'
-                width={30}
-                height={30}
-                className='rounded-full'
+                  src={"/logo_.png"}
+                  alt='logo'
+                  width={150}
+                  height={150}
               />
-            </Link>
-          )
-        }
-        {!currentUser && <Link href={"/login"} className="flex spacex-x-2 items-center">
-            <button className='text-sm bg-gray-semibold hover:bg-gray-light rounded-2xl md:px-12 px-6 py-1 text-white cursor-pointer'>sign in</button>
-        </Link>}
-        {
-          currentUser && <button onClick={() => dispatch(logout())} className='text-sm bg-gray-semibold hover:bg-gray-light rounded-2xl md:px-12 px-6 py-1 text-white cursor-pointer'>logout</button>
-        }
+          </Link>
+          {
+            
+              <div className="md:flex hidden items-center mx-auto md:space-x-6 text-sm space-x-2 text-white">
+                <Link href={"/home"}  className="flex space-x-2 items-center  focus:text-gray-semibold hover:text-gray-semibold">
+                  <CiHome size={"1.2em"}/>
+                  <span>Home</span>
+                </Link>
+                <Link href={"/search"} className="md:flex hidden space-x-2 items-center  focus:text-gray-semibold hover:text-gray-semibold">
+                  <IoIosSearch size={"1.2em"}/>
+                  <span>Search</span>
+                </Link>
+                {currentUser && <Link href={"/watch_list"} className="md:flex hidden space-x-2 items-center  focus:text-gray-semibold hover:text-gray-semibold">
+                  <FaPlus size={"1.2em"}/>
+                  <span>Watchlist</span>
+                </Link>}
+              </div>
+            
+          }
+          {
+            currentUser && (
+              <Link href={"/profile"} className="flex space-x-1 text-white items-center">
+                <span>My Profile</span>
+                <Image
+                  src={"/avatar.png"}
+                  alt='avatar'
+                  width={30}
+                  height={30}
+                  className='rounded-full'
+                />
+              </Link>
+            )
+          }
+          {!currentUser && <Link href={"/login"} className="flex spacex-x-2 items-center">
+              <button className='text-sm bg-gray-semibold hover:bg-gray-light rounded-2xl md:px-12 px-6 py-1 text-white cursor-pointer'>sign in</button>
+          </Link>}
+          {
+            currentUser && <button onClick={() => dispatch(logout())} className='text-sm bg-gray-semibold hover:bg-gray-light rounded-2xl md:px-12 px-6 py-1 text-white cursor-pointer'>logout</button>
+          }
+          
+      </div>
+      <div className="flex flex-col w-full space-y-2 text-center md:hidden">
+          <Link href={"/home"}  className="flex space-x-2 items-center  focus:text-gray-semibold hover:text-gray-semibold">
+                  <CiHome size={"1.2em"}/>
+                  <span>Home</span>
+          </Link>
+          <Link href={"/search"} className="md:flex hidden space-x-2 items-center  focus:text-gray-semibold hover:text-gray-semibold">
+                  <IoIosSearch size={"1.2em"}/>
+                  <span>Search</span>
+                </Link>
+          {currentUser && <Link href={"/watch_list"} className="md:flex hidden space-x-2 items-center  focus:text-gray-semibold hover:text-gray-semibold">
+            <FaPlus size={"1.2em"}/>
+            <span>Watchlist</span>
+          </Link>}
+      </div>
     </div>
+    
   )
 }
 
