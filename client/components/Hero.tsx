37c8c3722d1c90abdda222b0a4981f9b81bc1e22
Labann <labanmusen32@gmail.com>
2025-11-15 11:02:38 +0300

    ui-polished

--- client/components/Hero.tsx
+++ client/components/Hero.tsx
@@ -7,7 +7,7 @@ const Hero = () => {
         <div className="absolute top-0 left-0 h-[60vh] w-full bg-black/50 z-20"></div>
         <div className="max-w-lg mx-auto text-white space-y-6 flex z-40 flex-col items-center">
             <p className='text-3xl md:text-4xl font-extrabold text-wrap text-center'>Unlimited films, stay tuned and chilled</p>
-            <Link href="/sign_up" className='bg-gray-semibold hover:bg-gray-light px-8 py-2 text-sm md:text-sm rounded-3xl  cursor-pointer'>sign up now</Link>
+            <Link href="/sign_up" className='text-sm bg-gray-semibold hover:bg-gray-light rounded-2xl md:px-12 px-6 py-1 text-white cursor-pointer'>sign up now</Link>
         </div>
     </div>
   )
