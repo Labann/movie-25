1bab384fedbcddae92ff5ab8accdd252dac3b3de
Labann <labanmusen32@gmail.com>
2025-11-17 04:35:20 +0300

    project done

--- client/components/Movie_details.tsx
+++ client/components/Movie_details.tsx
@@ -21,34 +21,34 @@ const Movie_details = ({movie_id}: {movie_id: string}) => {
             <h3 className='text-gray-semibold'>More details</h3>
             <div className="grid lg:grid-cols-4 md:grid-cols-3 grid-cols-2 gap-3">
                 <div className="flex flex-col space-y-3">
-                    <div className="flex items-center space-x-2 text-gray-400 text-lg">
+                    <div className="flex items-center space-x-2 text-gray-400 text-md">
                         <IoCalendarClearOutline className=' font-semibold'/>
-                        <p className='capitalized text-lg'>Released Year</p>
+                        <p className='capitalized'>Released Year</p>
                     </div>
                     <p className='text-white font-extralight'>{movie?.release_date.slice(0, 4)}</p>
                 </div>
                 <div className="flex flex-col space-y-3">
-                    <div className="flex items-center space-x-2 text-gray-400 text-lg">
+                    <div className="flex items-center space-x-2 text-gray-400 text-md">
                         <IoLanguageOutline className=' font-semibold'/>
-                        <p className='capitalized text-lg'>Available languages</p>
+                        <p className='capitalized'>Available languages</p>
                     </div>
                     <div className="flex items-center space-x-2 text-white">
                         {<span className='bg-black rounded-md px-3 py-1 cursor-pointer text-sm'>{movie?.original_language}</span>} 
                     </div>
                 </div>
                 <div className="flex flex-col space-y-3">
-                    <div className="flex items-center space-x-2 text-gray-400 text-lg">
+                    <div className="flex items-center space-x-2 text-gray-400 text-md">
                         <LiaIconsSolid className=' font-semibold'/>
-                        <p className='capitalized text-lg'>Genres</p>
+                        <p className='capitalized text-md'>Genres</p>
                     </div>
                     <div className="flex items-center flex-wrap space-y-2 space-x-2 text-white">
                         {movie?.genres?.map(genre => <span key={genre.id} className='bg-black text-sm rounded-md px-3 py-1 cursor-pointer'>{genre.name}</span>)}
                     </div>
                 </div>
                 <div className="flex flex-col space-y-3">
-                    <div className="flex items-center space-x-2 text-gray-400 text-lg">
+                    <div className="flex items-center space-x-2 text-gray-400 text-md">
                         <GoStarFill className=''/>
-                        <p className='capitalized text-lg'>Popularity</p>
+                        <p className='capitalized text-md'>Popularity</p>
                     </div>
                     <div className="flex items-center space-x-2">
                         <span className='text-xs text-gray-light'>rating</span>
